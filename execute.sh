d029a9de131eda384e5689a5c23b8cfa4885c0ed
lifuhuang97 <54570187+lifuhuang97@users.noreply.github.com>
2020-04-07 02:25:25 +0800

    sync execute.sh (forgot to update this)

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-javac -d classes -cp src/mysql-connector-java-8.0.19.jar;. src/*.java
-java -cp src/mysql-connector-java-8.0.19.jar;classes SocialMagnetApp
+javac -d target/classes -cp src/main/java:lib src/main/java/*.java
+java -cp target/classes:lib SocialMagnetApp
