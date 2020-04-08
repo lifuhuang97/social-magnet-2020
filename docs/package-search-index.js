91c17cdafd58a37bdebfe200b417ac120dfa25e7
lifuhuang97 <54570187+lifuhuang97@users.noreply.github.com>
2020-04-08 22:12:27 +0800

    Generated javadocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"project.exception"},{"l":"project.magnet"},{"l":"project.utilities"}]
--- src/test/java/project/farm/TestPlot.java
+++ src/test/java/project/farm/TestPlot.java
@@ -7,6 +7,7 @@ import java.util.*;
 import project.farm.*;
 import project.magnet.UserProfile;
 import project.utilities.*;
+import static org.junit.Assert.*;
 
 
 /**
@@ -15,7 +16,52 @@ import project.utilities.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestPlot {
 
-    
+    private UserProfile testUser;
+    private CityFarmerCtrl ctrl;
+    private CityFarmerMenu farmMenu = new CityFarmerMenu();
+
+    @BeforeAll
+    public void resetUser(){
+
+        UserProfile checkIfExist = UserProfileDAO.getUserProfileByUsername("plottest");
+
+        if(checkIfExist != null){
+            DataUtility.queryUpdate("DELETE FROM USERPROFILE WHERE USERNAME = 'plottest'");
+        }
+        
+        if(UserProfileDAO.createUser("plottest","Plot Test", "plottest")){
+            testUser = UserProfileDAO.getUserProfileByUsername("plottest");
+            ctrl = new CityFarmerCtrl(testUser);
+        }
+
+    }
+
+    @Test
+    public void testIfStartWithZeroPlot(){
+
+        ArrayList<Plot> myPlots = PlotDAO.getMyPlots(testUser);
+
+        int expectedPlotSize = 0;
+
+        int myPlotSize = myPlots.size();
+
+        assertEquals(expectedPlotSize, myPlotSize);
+
+    }
+
+    @Test
+    public void testIfAutomaticGeneratePlotWhenFirstEnterFarm(){
+        CityFarmerCtrl ctrl = new CityFarmCtrl(testUser);
+
+        ArrayList<Plot> myPlots = PlotDAO.getMyPlots(testUser);
+
+        if()
+
+
+
+    }
+
+
 
 
     
